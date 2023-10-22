63a8e46e77a0ee31deddc50ff13661ba82a64282
devansh <suryakantlodhi74@gmail.com>
2023-10-22 22:23:37 +0530

    changed uri

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 const db = ()=>{
     try {
-        mongoose.connect(process.env.URI,
+        mongoose.connect(process.env.MONGODB_URI,
             {
                 dbName:'MusicPlayer'
             }
